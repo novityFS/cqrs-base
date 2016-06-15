a40b0f701837b96ea9293a8b07b9dd101e959bc0
Frank Seidinger <f.seidinger@prosoz.de>
2016-06-15 17:13:19 +0200

    Added documentation of root aggregate

--- src/main/java/de/novity/cqrs/base/api/RootAggregate.java
+++ src/main/java/de/novity/cqrs/base/api/RootAggregate.java
@@ -20,21 +20,60 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This is the base class of all root aggregates.
+ * <p/>
+ * An root aggregate is a special case of an entity and therefore has an unique identity. A root aggregate spans an
+ * object tree of other entities and value objects building a single unit, the aggregate. The root aggregate is build
+ * in such a way that it takes responsibility for the invariants of the whole unit. In technical terms the root
+ * aggregate is also responsible for maintaining the transactional border of the whole unit.
+ * <p/>
+ * If an executed command results in a state change of a root aggregate, the root aggregate creates the domain events,
+ * as the observable result of the state change. Usually your command handlers are responsible to publish these events
+ * to interested event receivers.
+ * <p/>
+ * Your root aggregate should extend this base class to get a functionality for managing the domain events created
+ * by your root aggregate.
+ *
+ * @See RootAggregate for an example of the cooperation between comand hanlder and root aggregate.
+ */
 public abstract class RootAggregate {
+    /**
+     * The list of events representing state changes of this root aggregate since the last commit.
+     */
     private final List<Event> events;
 
     protected RootAggregate() {
         this.events = new ArrayList<Event>();
     }
 
-    protected void addChange(Event event) {
+    /**
+     * Add an event to the list of uncommited state changes of this root aggregate.
+     *
+     * @param event The domain event representing a change of state of this root aggregate.
+     */
+    protected void addEvent(Event event) {
         events.add(event);
     }
 
+    /**
+     * Returns the list of uncommmited state changes of this root aggregate since the last commit.
+     * <p/>
+     * Usually this method is called by a command handler after the command handler has successfully called business
+     * methods of affected root aggregates to get their resulting state changes in form of events.
+     *
+     * @return List of uncommited state changes.
+     */
     public List<Event> getEvents() {
         return Collections.unmodifiableList(events);
     }
 
+    /**
+     * Commits the uncommited state changes of this root aggregate.
+     * <p/>
+     * Usually this method is called by a command handler after successfully publishing the events of affected root
+     * aggregates to interested event subscribers.
+     */
     public void commitEvents() {
         events.clear();
     }
